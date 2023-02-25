b1028ac913bf6c71548196d1eb235dd41f4609ba
Silvio Junior Carvalho de Araujo <54729104+s1lviuz@users.noreply.github.com>
2023-02-25 22:32:39 +0000

    Set a global Google Font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  if (metric.label === 'web-vital') {
+    console.log(metric)
+  }
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main className={`${inter.variable} font-sans`}><Component {...pageProps} /></main>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: [
+  content: [
     "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./util/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},

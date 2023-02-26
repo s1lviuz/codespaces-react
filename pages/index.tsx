3c9ee0a1bbf6d434d373a64c7f9c0dc7465b7c48
Silvio Junior Carvalho de Araujo <54729104+s1lviuz@users.noreply.github.com>
2023-02-26 09:08:12 +0000

    Stage commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,31 @@
 import React from "react";
-import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Button, Card, CardActions, CardContent, CardHeader } from '@mui/material';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 
 const HomePage: React.FC = () => {
-  return <div>Welcome to Next.js! <span>User page <Link href='/user'>here</Link></span></div>
+  const router = useRouter()
+
+  return <Card
+    variant="outlined"
+
+  >
+    <CardHeader
+      title="Bem vindo!"
+    />
+    <CardContent>
+
+    </CardContent>
+    <CardActions>
+      <Button
+        variant="outlined"
+        onClick={() => router.push('/user')}
+        startIcon={<PersonSearchIcon />}
+      >
+        Random User
+      </Button>
+    </CardActions>
+  </Card>
 }
 
 export default HomePage;
--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { GetStaticProps } from 'next'
 import { iUserProps } from "../../types";
+import { Card, CardContent, CardHeader } from '@mui/material';
 
 export const getStaticProps: GetStaticProps = async () => {
     const res = await fetch('https://random-data-api.com/api/v2/users')
     const userData: iUserProps = await res.json()
-
     return {
         props: userData
     }
 }
 
 const UserData: React.FC<iUserProps> = (userData: iUserProps) => {
+    console.log(userData)
     return (
-        <div>
-            <p>Name: {userData.first_name ?? "Não existe."}</p>
-            <p>Email: {userData.email ?? "Não possui."}</p>
-            <p>Number: {userData.phone_number ?? "Não informado."}</p>
-        </div>
+        <Card>
+            <CardHeader
+                avatar={<img src={userData.avatar} alt="Avatar" width={50} height={50} />}
+                title={userData.username}
+            />
+            <CardContent>
+                <p>Name: {userData.first_name} {userData.last_name}</p>
+                <p>Email: {userData.email}</p>
+                <p>Number: {userData.phone_number}</p>
+            </CardContent>
+        </Card>
     )
 }
 

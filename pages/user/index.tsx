8fe9a6cd1963ba2ad5150db4502250931db3ce50
Silvio Junior Carvalho de Araujo <54729104+s1lviuz@users.noreply.github.com>
2023-02-27 02:39:05 +0000

    Stage commit

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { GetStaticProps } from 'next'
 import { iUserProps } from "../../types";
-import { Card, CardContent, CardHeader, Container, Grid, Paper, Typography } from '@mui/material';
+import { Box, Card, CardContent, CardHeader, Container, Grid, Paper, Typography } from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
@@ -27,146 +27,115 @@ export const getStaticProps: GetStaticProps = async () => {
 
 const UserData: React.FC<iUserProps> = (userData: iUserProps) => {
     return (
-        <Card elevation={3}>
-            <CardHeader
-                avatar={<img src={userData.avatar} alt="Avatar" width={70} height={70} />}
-                title={userData.username}
-                titleTypographyProps={{ variant: "h5" }}
-                subheader={`#${userData.id}`}
-                subheaderTypographyProps={{ fontWeight: "bold" }}
-                component={Container}
-            />
-            <CardContent>
-                <Container>
-                    <Paper variant="outlined" sx={{ padding: "1%", }}>
-                        <Grid>
-                            <Typography fontWeight="bold" sx={{ marginBottom: "5px" }}><InfoIcon /> About:</Typography>
-                            <Grid container sx={{ gap: "5px" }}>
-                                <Paper variant="outlined" sx={{ padding: "1%" }}>
-                                    <Grid>
-                                        <Grid item xs>
-                                            <Typography fontWeight="bold"><PersonIcon /> Name:</Typography>
-                                        </Grid>
-                                        <Grid item xs>
-                                            <Typography>{userData.first_name} {userData.last_name}</Typography>
-                                        </Grid>
-                                    </Grid>
-                                </Paper>
-                                <Paper variant="outlined" sx={{ padding: "1%" }}>
-                                    <Grid>
-                                        <Grid item xs>
-                                            <Typography fontWeight="bold"><AlternateEmailIcon /> Email:</Typography>
-                                        </Grid>
-                                        <Grid item xs>
-                                            <Typography>{userData.email}</Typography>
-                                        </Grid>
-                                    </Grid>
-                                </Paper>
-                                <Paper variant="outlined" sx={{ padding: "1%" }}>
-                                    <Grid>
-                                        <Grid item xs>
-                                            <Typography fontWeight="bold"><LocalPhoneIcon /> Number:</Typography>
-                                        </Grid>
-                                        <Grid item xs>
-                                            <Typography>{userData.phone_number}</Typography>
-                                        </Grid>
-                                    </Grid>
-                                </Paper>
-                                <Paper variant="outlined" sx={{ padding: "1%" }}>
-                                    <Grid>
-                                        <Grid item xs>
-                                            <Typography fontWeight="bold"><AssignmentIndIcon /> Social Number:</Typography>
-                                        </Grid>
-                                        <Grid item xs>
-                                            <Typography>{userData.social_insurance_number}</Typography>
-                                        </Grid>
-                                    </Grid>
-                                </Paper>
-                                <Paper variant="outlined" sx={{ padding: "1%" }}>
-                                    <Grid>
-                                        <Grid item xs>
-                                            <Typography fontWeight="bold"><TransgenderIcon /> Gender:</Typography>
-                                        </Grid>
-                                        <Grid item xs>
-                                            <Typography>{userData.gender}</Typography>
-                                        </Grid>
-                                    </Grid>
-                                </Paper>
-                                <Paper variant="outlined" sx={{ padding: "1%" }}>
-                                    <Grid>
-                                        <Grid item xs>
-                                            <Typography fontWeight="bold"><CakeIcon /> Birth date:</Typography>
-                                        </Grid>
-                                        <Grid item xs>
-                                            <Typography>{moment(userData.date_of_birth).format('L')}</Typography>
-                                        </Grid>
-                                    </Grid>
-                                </Paper>
-                                <Paper variant="outlined" sx={{ padding: "1%" }}>
-                                    <Grid>
-                                        <Grid item xs>
-                                            <Typography fontWeight="bold"><BadgeIcon /> Employment:</Typography>
-                                        </Grid>
-                                        <Grid item xs>
-                                            <Typography>{userData.employment.title}</Typography>
-                                        </Grid>
-                                    </Grid>
-                                </Paper>
+        <Container sx={{ paddingTop: "15vh", justifyItems: "center" }}>
+            <Card elevation={8}>
+                <CardHeader
+                    avatar={<img src={userData.avatar} alt="Avatar" width={100} height={100} />}
+                    title={userData.username}
+                    titleTypographyProps={{ variant: "h5" }}
+                    subheader={`#${userData.id}`}
+                    subheaderTypographyProps={{ fontWeight: "bold" }}
+                    component={Container}
+                />
+                <CardContent component={Container}>
+                    <Card variant="outlined" sx={{ marginBottom: "10px" }}>
+                        <CardHeader
+                            title="About:"
+                            titleTypographyProps={{ fontWeight: "bold", variant: "h6" }}
+                            avatar={<InfoIcon />}
+                            component={Container}
+                        />
+                        <CardContent
+                            component={Container}
+                        >
+                            <Grid container spacing={2} columns={9}>
+                                <Grid item xs={3}>
+                                    <Paper variant="outlined" sx={{ padding: "5%" }}>
+                                        <Typography fontWeight="bold"><PersonIcon /> Name:</Typography>
+                                        <Typography>{userData.first_name} {userData.last_name}</Typography>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Paper variant="outlined" sx={{ padding: "5%" }}>
+                                        <Typography fontWeight="bold"><AlternateEmailIcon /> Email:</Typography>
+                                        <Typography>{userData.email}</Typography>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Paper variant="outlined" sx={{ padding: "5%" }}>
+                                        <Typography fontWeight="bold"><LocalPhoneIcon /> Number:</Typography>
+                                        <Typography>{userData.phone_number}</Typography>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Paper variant="outlined" sx={{ padding: "5%" }}>
+                                        <Typography fontWeight="bold"><AssignmentIndIcon /> Social Number:</Typography>
+                                        <Typography>{userData.social_insurance_number}</Typography>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Paper variant="outlined" sx={{ padding: "5%" }}>
+                                        <Typography fontWeight="bold"><TransgenderIcon /> Gender:</Typography>
+                                        <Typography>{userData.gender}</Typography>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Paper variant="outlined" sx={{ padding: "5%" }}>
+                                        <Typography fontWeight="bold"><CakeIcon /> Birth date:</Typography>
+                                        <Typography>{moment(userData.date_of_birth).format('L')}</Typography>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Paper variant="outlined" sx={{ padding: "5%" }}>
+                                        <Typography fontWeight="bold"><BadgeIcon /> Employment:</Typography>
+                                        <Typography>{userData.employment.title}</Typography>
+                                    </Paper>
+                                </Grid>
+
                             </Grid>
-                        </Grid>
-                    </Paper>
-                    <Paper variant="outlined" sx={{ padding: "1%", marginTop: "5px" }}>
-                        <Grid>
-                            <Typography fontWeight="bold" sx={{ marginBottom: "5px" }}><LocalActivityIcon /> Subscription:</Typography>
-                            <Grid item xs>
-                                <Grid container sx={{ gap: "5px" }}>
-                                    <Paper variant="outlined" sx={{ padding: "1%" }}>
-                                        <Grid>
-                                            <Grid item xs>
-                                                <Typography fontWeight="bold"><CardMembershipIcon /> Plan:</Typography>
-                                            </Grid>
-                                            <Grid item xs>
-                                                <Typography>{userData.subscription.plan}</Typography>
-                                            </Grid>
-                                        </Grid>
+                        </CardContent>
+                    </Card>
+                    <Card variant="outlined">
+                        <CardHeader
+                            title="Subscription:"
+                            titleTypographyProps={{ fontWeight: "bold", variant: "h6" }}
+                            avatar={<LocalActivityIcon />}
+                            component={Container}
+                        />
+                        <CardContent
+                            component={Container}
+                        >
+                            <Grid container spacing={2} columns={4}>
+                                <Grid item xs={1}>
+                                    <Paper variant="outlined" sx={{ padding: "5%" }}>
+                                        <Typography fontWeight="bold"><CardMembershipIcon /> Plan:</Typography>
+                                        <Typography>{userData.subscription.plan}</Typography>
                                     </Paper>
-                                    <Paper variant="outlined" sx={{ padding: "1%" }}>
-                                        <Grid>
-                                            <Grid item xs>
-                                                <Typography fontWeight="bold"><ChangeCircleIcon /> Status:</Typography>
-                                            </Grid>
-                                            <Grid item xs>
-                                                <Typography>{userData.subscription.status}</Typography>
-                                            </Grid>
-                                        </Grid>
+                                </Grid>
+                                <Grid item xs={1}>
+                                    <Paper variant="outlined" sx={{ padding: "5%" }}>
+                                        <Typography fontWeight="bold"><ChangeCircleIcon /> Status:</Typography>
+                                        <Typography>{userData.subscription.status}</Typography>
                                     </Paper>
-                                    <Paper variant="outlined" sx={{ padding: "1%" }}>
-                                        <Grid>
-                                            <Grid item xs>
-                                                <Typography fontWeight="bold"><PaidIcon /> Payment method:</Typography>
-                                            </Grid>
-                                            <Grid item xs>
-                                                <Typography>{userData.subscription.payment_method}</Typography>
-                                            </Grid>
-                                        </Grid>
+                                </Grid>
+                                <Grid item xs={1}>
+                                    <Paper variant="outlined" sx={{ padding: "5%" }}>
+                                        <Typography fontWeight="bold"><PaidIcon /> Payment method:</Typography>
+                                        <Typography>{userData.subscription.payment_method}</Typography>
                                     </Paper>
-                                    <Paper variant="outlined" sx={{ padding: "1%" }}>
-                                        <Grid>
-                                            <Grid item xs>
-                                                <Typography fontWeight="bold"><GavelIcon /> Term:</Typography>
-                                            </Grid>
-                                            <Grid item xs>
-                                                <Typography>{userData.subscription.term}</Typography>
-                                            </Grid>
-                                        </Grid>
+                                </Grid>
+                                <Grid item xs={1}>
+                                    <Paper variant="outlined" sx={{ padding: "5%" }}>
+                                        <Typography fontWeight="bold"><GavelIcon /> Term:</Typography>
+                                        <Typography>{userData.subscription.term}</Typography>
                                     </Paper>
                                 </Grid>
                             </Grid>
-                        </Grid>
-                    </Paper>
-                </Container>
-            </CardContent>
-        </Card>
+                        </CardContent>
+                    </Card>
+                </CardContent>
+            </Card>
+        </Container>
     )
 }
 

29cee59f87c5421e1f6d0bd78aa06f7d2033769a
Silvio Junior Carvalho de Araujo <54729104+s1lviuz@users.noreply.github.com>
2023-02-27 04:38:14 +0000

    Fixed "Random User" page

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { GetStaticProps } from 'next'
 import { iUserProps } from "../../types";
-import { Box, Card, CardContent, CardHeader, Container, Grid, Paper, Typography } from '@mui/material';
+import { Button, Card, CardContent, CardHeader, Container, Grid, Paper, Typography } from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
@@ -15,6 +15,7 @@ import PaidIcon from '@mui/icons-material/Paid';
 import GavelIcon from '@mui/icons-material/Gavel';
 import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
 import InfoIcon from '@mui/icons-material/Info';
+import CodeIcon from '@mui/icons-material/Code';
 import moment from "moment";
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -27,115 +28,127 @@ export const getStaticProps: GetStaticProps = async () => {
 
 const UserData: React.FC<iUserProps> = (userData: iUserProps) => {
     return (
-        <Container sx={{ paddingTop: "15vh", justifyItems: "center" }}>
-            <Card elevation={8}>
+        <Container>
+            <Card elevation={8} component={Container}>
                 <CardHeader
                     avatar={<img src={userData.avatar} alt="Avatar" width={100} height={100} />}
                     title={userData.username}
                     titleTypographyProps={{ variant: "h5" }}
                     subheader={`#${userData.id}`}
                     subheaderTypographyProps={{ fontWeight: "bold" }}
-                    component={Container}
                 />
-                <CardContent component={Container}>
-                    <Card variant="outlined" sx={{ marginBottom: "10px" }}>
+                <CardContent>
+                    <Card component={Container} variant="outlined" sx={{ marginBottom: "10px" }}>
                         <CardHeader
                             title="About:"
                             titleTypographyProps={{ fontWeight: "bold", variant: "h6" }}
                             avatar={<InfoIcon />}
-                            component={Container}
                         />
                         <CardContent
-                            component={Container}
                         >
                             <Grid container spacing={2} columns={9}>
                                 <Grid item xs={3}>
                                     <Paper variant="outlined" sx={{ padding: "5%" }}>
                                         <Typography fontWeight="bold"><PersonIcon /> Name:</Typography>
-                                        <Typography>{userData.first_name} {userData.last_name}</Typography>
+                                        <Typography textAlign="center">{userData.first_name} {userData.last_name}</Typography>
                                     </Paper>
                                 </Grid>
                                 <Grid item xs={3}>
                                     <Paper variant="outlined" sx={{ padding: "5%" }}>
                                         <Typography fontWeight="bold"><AlternateEmailIcon /> Email:</Typography>
-                                        <Typography>{userData.email}</Typography>
+                                        <Typography textAlign="center">{userData.email}</Typography>
                                     </Paper>
                                 </Grid>
                                 <Grid item xs={3}>
                                     <Paper variant="outlined" sx={{ padding: "5%" }}>
                                         <Typography fontWeight="bold"><LocalPhoneIcon /> Number:</Typography>
-                                        <Typography>{userData.phone_number}</Typography>
+                                        <Typography textAlign="center">{userData.phone_number}</Typography>
                                     </Paper>
                                 </Grid>
                                 <Grid item xs={3}>
                                     <Paper variant="outlined" sx={{ padding: "5%" }}>
                                         <Typography fontWeight="bold"><AssignmentIndIcon /> Social Number:</Typography>
-                                        <Typography>{userData.social_insurance_number}</Typography>
+                                        <Typography textAlign="center">{userData.social_insurance_number}</Typography>
                                     </Paper>
                                 </Grid>
                                 <Grid item xs={3}>
                                     <Paper variant="outlined" sx={{ padding: "5%" }}>
                                         <Typography fontWeight="bold"><TransgenderIcon /> Gender:</Typography>
-                                        <Typography>{userData.gender}</Typography>
+                                        <Typography textAlign="center">{userData.gender}</Typography>
                                     </Paper>
                                 </Grid>
                                 <Grid item xs={3}>
                                     <Paper variant="outlined" sx={{ padding: "5%" }}>
                                         <Typography fontWeight="bold"><CakeIcon /> Birth date:</Typography>
-                                        <Typography>{moment(userData.date_of_birth).format('L')}</Typography>
+                                        <Typography textAlign="center">{moment(userData.date_of_birth).format('L')}</Typography>
                                     </Paper>
                                 </Grid>
                                 <Grid item xs={3}>
                                     <Paper variant="outlined" sx={{ padding: "5%" }}>
                                         <Typography fontWeight="bold"><BadgeIcon /> Employment:</Typography>
-                                        <Typography>{userData.employment.title}</Typography>
+                                        <Typography textAlign="center">{userData.employment.title}</Typography>
                                     </Paper>
                                 </Grid>
 
                             </Grid>
                         </CardContent>
                     </Card>
-                    <Card variant="outlined">
+                    <Card component={Container} variant="outlined">
                         <CardHeader
                             title="Subscription:"
                             titleTypographyProps={{ fontWeight: "bold", variant: "h6" }}
                             avatar={<LocalActivityIcon />}
-                            component={Container}
                         />
                         <CardContent
-                            component={Container}
                         >
                             <Grid container spacing={2} columns={4}>
                                 <Grid item xs={1}>
                                     <Paper variant="outlined" sx={{ padding: "5%" }}>
                                         <Typography fontWeight="bold"><CardMembershipIcon /> Plan:</Typography>
-                                        <Typography>{userData.subscription.plan}</Typography>
+                                        <Typography textAlign="center">{userData.subscription.plan}</Typography>
                                     </Paper>
                                 </Grid>
                                 <Grid item xs={1}>
                                     <Paper variant="outlined" sx={{ padding: "5%" }}>
                                         <Typography fontWeight="bold"><ChangeCircleIcon /> Status:</Typography>
-                                        <Typography>{userData.subscription.status}</Typography>
+                                        <Typography textAlign="center">{userData.subscription.status}</Typography>
                                     </Paper>
                                 </Grid>
                                 <Grid item xs={1}>
                                     <Paper variant="outlined" sx={{ padding: "5%" }}>
                                         <Typography fontWeight="bold"><PaidIcon /> Payment method:</Typography>
-                                        <Typography>{userData.subscription.payment_method}</Typography>
+                                        <Typography textAlign="center">{userData.subscription.payment_method}</Typography>
                                     </Paper>
                                 </Grid>
                                 <Grid item xs={1}>
                                     <Paper variant="outlined" sx={{ padding: "5%" }}>
                                         <Typography fontWeight="bold"><GavelIcon /> Term:</Typography>
-                                        <Typography>{userData.subscription.term}</Typography>
+                                        <Typography textAlign="center">{userData.subscription.term}</Typography>
                                     </Paper>
                                 </Grid>
                             </Grid>
                         </CardContent>
                     </Card>
+                    <Button
+                        href="https://github.com/s1lviuz/codespaces-react"
+                        variant="contained"
+                        target="_blank"
+                        color="primary"
+                        endIcon={<CodeIcon />}
+                        sx={{
+                            background: "black",
+                            '&:hover': {
+                                backgroundColor: 'black',
+                            },
+                            marginTop: "10px"
+                        }}
+                    >
+                        Source code
+                    </Button>
                 </CardContent>
             </Card>
         </Container>
+
     )
 }
 

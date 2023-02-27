93b84649d9a2caf7f63bf0c4fb363cddf41b8f1e
Silvio Junior Carvalho de Araujo <54729104+s1lviuz@users.noreply.github.com>
2023-02-27 04:55:08 +0000

    Added index menu layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,35 @@
 import React from "react";
 import { useRouter } from 'next/router';
-import { Button, Card, CardActions, CardContent, CardHeader } from '@mui/material';
+import { Button, Card, CardActions, CardContent, CardHeader, Container } from '@mui/material';
 import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 
 const HomePage: React.FC = () => {
   const router = useRouter()
 
-  return <Card
-    variant="outlined"
+  return <Container maxWidth="sm">
+    <Card
+      component={Container}
+      elevation={10}
+    >
+      <CardHeader
+        title="😁 Welcome! 😁"
+        titleTypographyProps={{ textAlign: "center" }}
+      />
+      <CardContent>
 
-  >
-    <CardHeader
-      title="Bem vindo!"
-    />
-    <CardContent>
-
-    </CardContent>
-    <CardActions>
-      <Button
-        variant="outlined"
-        onClick={() => router.push('/user')}
-        startIcon={<PersonSearchIcon />}
-      >
-        Random User
-      </Button>
-    </CardActions>
-  </Card>
+      </CardContent>
+      <CardActions>
+        <Button
+          variant="outlined"
+          onClick={() => router.push('/user')}
+          startIcon={<PersonSearchIcon />}
+          sx={{ marginBottom: "10px", color: "black", borderColor: "black", "&:hover": { borderColor: "black" } }}
+        >
+          Random User
+        </Button>
+      </CardActions>
+    </Card>
+  </Container>
 }
 
 export default HomePage;
